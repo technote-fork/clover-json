6eb68a9eba4244a8fde4eba944b8e7a89a8fb684
Technote <technote.space@gmail.com>
2020-02-11 22:44:37 +0900

    chore: tweaks

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { parseString } from 'xml2js';
 
 type coverageData = { file: { $; line }[] }
 const getCoverageData = (projectObj): coverageData[] => Object.prototype.hasOwnProperty.call(projectObj, 'package') ? projectObj.package : [Object.assign({}, projectObj, {$: Object.assign({}, projectObj.$, {name: undefined})})];
-const getPackageName = (data): string => Object.prototype.hasOwnProperty.call(data, '$') && Object.prototype.hasOwnProperty.call(data.$, 'name') && data.$.name ? data.$.name + '/' : '';
+const getPackageName  = (data): string => Object.prototype.hasOwnProperty.call(data, '$') && Object.prototype.hasOwnProperty.call(data.$, 'name') && data.$.name ? data.$.name + '/' : '';
 
 type classDetail = { name?: string; fileName: string; lines?: { $: { type; name; num; count } }[] }
 const classDetailsFromProjects = (projects: []): classDetail[] => {
@@ -30,57 +30,50 @@ const classDetailsFromProjects = (projects: []): classDetail[] => {
 			});
 		});
 	});
+
 	return classDetails;
 };
 
 type methodStat = { name: string; line: number; hit: number }
 type lineStat = { line: number; hit: number }
 type classCov = { title?: string; file: string; functions: { found: number; hit: number; details: methodStat[] }; lines: { found: number; hit: number; details: lineStat[] } }
-const unpackage = (projects): classCov[] => {
-	const classDetails = classDetailsFromProjects(projects);
+const unpackage = (projects): classCov[] => classDetailsFromProjects(projects).map(detail => {
+	const methodStats: methodStat[] = detail.lines ? detail.lines.filter(line => line.$.type === 'method').map(line => ({
+		name: line.$.name,
+		line: Number(line.$.num),
+		hit: Number(line.$.count),
+	})) : [];
+	const lineStats: lineStat[]     = detail.lines ? detail.lines.filter(line => line.$.type !== 'method').map(line => ({
+		line: Number(line.$.num),
+		hit: Number(line.$.count),
+	})) : [];
 
-	return classDetails.map(detail => {
-		const methodStats: methodStat[] = detail.lines ? detail.lines.filter(line => line.$.type === 'method').map(line => ({
-			name: line.$.name,
-			line: Number(line.$.num),
-			hit: Number(line.$.count),
-		})) : [];
-		const lineStats: lineStat[] = detail.lines ? detail.lines.filter(line => line.$.type !== 'method').map(line => ({
-			line: Number(line.$.num),
-			hit: Number(line.$.count),
-		})) : [];
+	return {
+		title: detail.name,
+		file: detail.fileName,
+		functions: {
+			found: methodStats.length,
+			// eslint-disable-next-line no-magic-numbers
+			hit: methodStats.filter(val => val.hit > 0).length,
+			details: methodStats,
+		},
+		lines: {
+			found: lineStats.length,
+			// eslint-disable-next-line no-magic-numbers
+			hit: lineStats.filter(val => val.hit > 0).length,
+			details: lineStats,
+		},
+	};
+});
 
-		return {
-			title: detail.name,
-			file: detail.fileName,
-			functions: {
-				found: methodStats.length,
-				// eslint-disable-next-line no-magic-numbers
-				hit: methodStats.filter(val => val.hit > 0).length,
-				details: methodStats,
-			},
-			lines: {
-				found: lineStats.length,
-				// eslint-disable-next-line no-magic-numbers
-				hit: lineStats.filter(val => val.hit > 0).length,
-				details: lineStats,
-			},
-		};
+export const parseContent = (xml: string): Promise<classCov[]> => new Promise((resolve, reject): void => {
+	parseString(xml, (err, parseResult) => {
+		if (err) {
+			reject(err);
+		} else {
+			resolve(unpackage(parseResult.coverage.project));
+		}
 	});
-};
+});
 
-export const parseContent = (xml: string): Promise<classCov[]> => {
-	return new Promise((resolve, reject): void => {
-		parseString(xml, (err, parseResult) => {
-			if (err) {
-				reject(err);
-			} else {
-				resolve(unpackage(parseResult.coverage.project));
-			}
-		});
-	});
-};
-
-export const parseFile = async(filePath: string): Promise<classCov[]> => {
-	return await parseContent(await promisify(fs.readFile)(filePath, 'utf-8'));
-};
+export const parseFile = async(filePath: string): Promise<classCov[]> => await parseContent(await promisify(fs.readFile)(filePath, 'utf-8'));
